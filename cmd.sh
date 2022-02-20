49e0bd047f07d0c97416ebbadc50cd39d3db5716
Eric Jaslet <contact@ericjaslet.fr>
2022-02-20 22:01:58 +0100

    [DEV] : mise en place des condition du script pour les variables

--- cmd.sh
+++ cmd.sh
@@ -1,3 +1,45 @@
-#!/bin/bah
+#!/bin/bash
 
-echo "hello world"
+################################################################
+#
+# Desctiption : déploiement à la volée de container docker
+#
+# Auteur : Eric
+# Date : 19/02/2022
+#
+################################################################
+
+myvar="Eric"
+myage=44
+
+echo "$#"
+read -d '' options <<"BLOCK"
+Options :
+    - --create : lancer des conteneurs
+    - --start : redemarage des conteneurs
+    - --drop : supprimer les conteneurs créer par le deploy.sh
+    - --infos : caractéristiques des conteneurs (ip, nom, uer...)
+    - --ansible : deploiement arborescence ansible
+
+cmd : 
+    - -z : variable vide
+    - -f : fichier existe
+    - -d : répertoire existe 
+BLOCK
+
+if [ "$1" == "--create" ];then
+    echo "notre option est --create"
+elif [ "$1" == "--start" ];then
+    echo "option --start"
+elif [ "$1" == "--drop" ];then
+    echo "option --drop"
+elif [ "$1" == "--infos" ];then
+    echo "option --infos"
+elif [ "$1" == "--ansible" ];then
+    echo "option --ansible"
+else
+    echo "$options"
+fi
+
+
+
