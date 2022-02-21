8c69c035ae1f8e906d54564845fe09533df34ad1
Eric Jaslet <contact@ericjaslet.fr>
2022-02-21 22:49:33 +0100

    [DEV]: 	Ajout de la création d'un container 	Ajout du drop du container 	Traitement du 2ème paramètre

--- cmd.sh
+++ cmd.sh
@@ -27,19 +27,36 @@ cmd :
     - -d : répertoire existe 
 BLOCK
 
+#si option --create
 if [ "$1" == "--create" ];then
-    echo "notre option est --create"
+    echo "notre option est --create "
+    nb_machine=1
+    re='^[0-9]+$'
+    if [[ $2 =~ $re  &&  "$2" > 0 ]]; then #double crocher obligatoire pour re
+        echo "est un nombre"
+        [ "$2" != "" ] && nb_machine=$2
+
+        docker run -tid --name $USER-alpine alpine:latest
+
+        echo "nombre de container créé : ${nb_machine}"
+    fi
+
+#si option --start
 elif [ "$1" == "--start" ];then
     echo "option --start"
+
+#si option --drop
 elif [ "$1" == "--drop" ];then
     echo "option --drop"
+    docker rm -f $USER-alpine
+
+#si option --infos
 elif [ "$1" == "--infos" ];then
-    echo "option --infos"
+    echo "github depot Eric jaslet"
+
+#si option --ansible
 elif [ "$1" == "--ansible" ];then
     echo "option --ansible"
 else
     echo "$options"
 fi
-
-
-
